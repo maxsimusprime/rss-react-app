df1c7aa57a0fef5f3b38a3892286202e64010e0b
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-10-30 23:11:43 +0300

    feat: add Loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+export default class extends Component {
+  render() {
+    return (
+      <div className='loading'>
+        <img src="/assets/images/spinner.gif" alt="spinner" />
+      </div>
+    )
+  }
+}
