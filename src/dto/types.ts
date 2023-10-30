5e3ac7b2661e7c177751c4c0363eef25dabe6c96
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-10-30 18:19:23 +0300

    feat: add AstronomicalObject type

---
+++ src/dto/types.ts
@@ -0,0 +1,11 @@
+type AstronomicalObject = {
+  "uid": string;
+  "name": string;
+  "astronomicalObjectType": string;
+  "location": {
+    "uid": string;
+    "name": string;
+  }
+};
+
+export type { AstronomicalObject };
