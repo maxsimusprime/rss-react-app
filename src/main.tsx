db7f5b915db460568a1cd7cedf83e648e9426495
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-05 21:25:48 +0300

    feat: implement router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,33 @@
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import App from './App.tsx';
+import Details from './components/Details/Details.tsx';
 import ErrorBoundary from './ErrorBoundary.tsx';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<App />}
+    >
+      <Route
+        path="/"
+        element={<Details />}
+      />
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
+  <React.StrictMode>
+    <ErrorBoundary>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
