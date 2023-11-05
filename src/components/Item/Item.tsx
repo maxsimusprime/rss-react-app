586f812cb77e17a4387eab25a14be30493c9ecae
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-05 21:24:25 +0300

    feat: add Image component

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,54 +1,26 @@
 import './item.css';
-import type {
-  AstronomicalObject,
-  AstronomicalObjectType,
-} from '../../dto/types';
-
-const imageSrc = (type: AstronomicalObjectType): string => {
-  switch (type) {
-    case 'COMET':
-      return '/assets/images/comet.png';
-      break;
-    case 'GALAXY':
-      return '/assets/images/galaxy.png';
-      break;
-    case 'NEBULA':
-      return '/assets/images/nebula.png';
-      break;
-    case 'PLANET':
-      return '/assets/images/planet.png';
-      break;
-    case 'REGION':
-      return '/assets/images/region.png';
-      break;
-    case 'SECTOR':
-      return '/assets/images/sector.png';
-      break;
-    case 'STAR_SYSTEM':
-      return '/assets/images/star-system.png';
-      break;
-    default:
-      return '/assets/images/galaxy.png';
-      break;
-  }
-};
+import type { AstronomicalObject } from '../../dto/types';
+import { NavLink, useLocation } from 'react-router-dom';
+import Image from './Image';
 
 export default function Item(props: AstronomicalObject) {
-  const { name, astronomicalObjectType } = props;
+  const { uid, name, astronomicalObjectType } = props;
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  if (params.has('details')) params.delete('details');
+  params.append('details', uid);
 
   return (
-    <div className="item">
-      <div className="item__logo">
-        <img
-          src={imageSrc(astronomicalObjectType)}
-          alt="item-logo"
-          className="item__image"
-        />
+    <NavLink to={`?${params.toString()}`}>
+      <div className="item">
+        <div className="item__logo">
+          <Image astronomicalObjectType={astronomicalObjectType} />
+        </div>
+        <div className="item__title">name:</div>
+        <div className="item__title">{name}</div>
+        <div className="item__desc">description:</div>
+        <div className="item__desc">{astronomicalObjectType}</div>
       </div>
-      <div className="item__title">name:</div>
-      <div className="item__title">{name}</div>
-      <div className="item__desc">description:</div>
-      <div className="item__desc">{astronomicalObjectType}</div>
-    </div>
+    </NavLink>
   );
 }
