cd5359ea1f17fc37c9d5964ff85ec0b2c1c1cac0
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-05 21:28:10 +0300

    feat: add Pagination component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,15 +1,22 @@
-import type { AstronomicalObject } from '../../dto/types';
+import type { AstronomicalObject, Page } from '../../dto/types';
 import Item from '../Item/Item';
-import './list.css';
+import Pagination from '../Pagination/Pagination';
+import styles from './list.module.css';
 
-export default function List(props: { items: AstronomicalObject[] }) {
-  const { items } = props;
+export default function List(props: {
+  items: AstronomicalObject[];
+  page: Page | undefined;
+}) {
+  const { items, page } = props;
 
   return (
-    <div className="list">
-      {items.length <= 0
-        ? 'Items Not Found'
-        : items.map((item) => <Item {...item} key={item.uid} />)}
+    <div className={styles.wrapper}>
+      {page && page.totalElements > 0 && <Pagination page={page} />}
+      <div className={styles.list}>
+        {items.length <= 0
+          ? 'Items Not Found'
+          : items.map((item) => <Item {...item} key={item.uid} />)}
+      </div>
     </div>
   );
 }
