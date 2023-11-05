2a65f8bc70cd0726f8533260d0d00e1b77152b7c
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-06 01:10:54 +0300

    feat: add click on List closes Details component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,7 +1,9 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import type { AstronomicalObject, Page } from '../../dto/types';
 import Item from '../Item/Item';
 import Pagination from '../Pagination/Pagination';
 import styles from './list.module.css';
+import type { MouseEvent } from 'react';
 
 export default function List(props: {
   items: AstronomicalObject[];
@@ -9,8 +11,22 @@ export default function List(props: {
 }) {
   const { items, page } = props;
 
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const listClickHandle = (e: MouseEvent) => {
+    e.preventDefault();
+    const closeLink = new URLSearchParams(location.search);
+    const target = e.target;
+
+    if (target instanceof HTMLDivElement && closeLink.has('details')) {
+      closeLink.delete('details');
+      navigate(`/?${closeLink.toString()}`);
+    }
+  };
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} onClick={(e) => listClickHandle(e)}>
       {page && page.totalElements > 0 && <Pagination page={page} />}
       <div className={styles.list}>
         {items.length <= 0
