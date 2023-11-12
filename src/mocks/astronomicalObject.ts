f5e10b47b491cde41cb009c5d9968991460c25e4
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-12 17:21:47 +0300

    feat: add mock for astronomicalObject

---
+++ src/mocks/astronomicalObject.ts
@@ -0,0 +1,13 @@
+import { AstronomicalObject } from '../dto/types';
+
+export const astronomicalObject: AstronomicalObject = {
+  uid: 'ASMA0000288988',
+  name: '39 Serpentis',
+  astronomicalObjectType: 'STAR_SYSTEM',
+  location: {
+    uid: 'ASMA0000025892',
+    name: 'Alpha Quadrant',
+  },
+};
+
+export default astronomicalObject;
