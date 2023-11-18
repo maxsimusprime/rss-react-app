b852643d6dd56793613dfbe3a468acb712c7b47f
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-18 17:57:38 +0300

    feat: update List component test

--- src/__test__/List.test.tsx
+++ src/__test__/List.test.tsx
@@ -1,62 +1,49 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { describe, it, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, it } from 'vitest';
 import List from '../components/List/List';
 import { MemoryRouter } from 'react-router-dom';
-import { astronomicalObjects, page } from '../mocks/objects';
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
+import { server } from '../mocks/server';
+import { astronomicalObjectBaseResponseHandle, nullAstronomicalObjectBaseResponseHandle } from '../mocks/handlers';
 
-const { mockedContext } = vi.hoisted(() => {
-  return { mockedContext: vi.fn() };
-});
+describe('List (Card List) component', async () => {
 
-vi.mock('react-router-dom', async (importOriginal) => {
-  const module: object = await importOriginal();
-  return {
-    ...module,
-    useLoaderData: vi.fn(),
-  };
-});
-
-vi.mock('react', async (importOriginal) => {
-  const module: object = await importOriginal();
-  return {
-    ...module,
-    useContext: mockedContext,
-  };
-});
-
-describe('List (Card List) component', () => {
   it('component renders the specified number of cards', async () => {
-    mockedContext.mockReturnValue({
-      items: astronomicalObjects,
-      page,
-    });
+    server.use(astronomicalObjectBaseResponseHandle);
+    const countOfItemsOnPage = 10;
+    
     render(
-      <MemoryRouter>
-        <List />
-      </MemoryRouter>
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/?']}>
+          <List />
+        </MemoryRouter>
+      </Provider>
     );
 
-    const countOfItemsOnPage = 10;
-
-    const items = screen
-      .getAllByRole('link')
-      .filter((item) => item.classList.contains('item__link'));
-
-    expect(items.length).toBe(countOfItemsOnPage);
+    await waitFor(() => {
+      const items = screen
+        .getAllByRole('link')
+        .filter((item) => item.classList.contains('item__link'));
+  
+      expect(items.length).toBe(countOfItemsOnPage);
+    });
   });
 
   it('appropriate message is displayed if no cards are present', async () => {
-    mockedContext.mockReturnValue({
-      items: [],
-      page: { totalElements: 0 },
-    });
+    server.use(nullAstronomicalObjectBaseResponseHandle);
+
     render(
-      <MemoryRouter>
-        <List />
-      </MemoryRouter>
+      <Provider store={store}>
+        <MemoryRouter>
+          <List />
+        </MemoryRouter>
+      </Provider>
     );
 
-    expect(screen.getByText('Items Not Found')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('Items Not Found')).toBeInTheDocument();
+    });
   });
 });
