5b1b65585325425eb191745c1dd21612145cfd47
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-19 11:42:06 +0300

    feat: upadte Details tests

--- src/__test__/Details.test.tsx
+++ src/__test__/Details.test.tsx
@@ -1,102 +1,83 @@
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { describe, it, vi } from 'vitest';
+import { describe, it } from 'vitest';
 import Details from '../components/Details/Details';
-import {
-  MemoryRouter,
-  RouterProvider,
-  createMemoryRouter,
-} from 'react-router-dom';
-import astronomicalObject from '../mocks/astronomicalObject';
-import { detailsLoader } from '../components/Details/Details';
+import { Outlet, RouterProvider, createMemoryRouter } from 'react-router-dom';
 import App from '../App';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+import { server } from '../mocks/server';
 
-const { mockedUseNavigation } = vi.hoisted(() => {
-  return { mockedUseNavigation: vi.fn() };
-});
-
-vi.mock('react-router-dom', async (importOriginal) => {
-  const module: object = await importOriginal();
-  return {
-    ...module,
-    useLoaderData: () => astronomicalObject,
-    useNavigation: mockedUseNavigation,
-  };
-});
-
-describe('Details (Detailed Card) component', async () => {
-  it('loading indicator is displayed while fetching data', () => {
-    mockedUseNavigation.mockReturnValue({ state: 'loading' });
-    render(
-      <MemoryRouter initialEntries={['/?page=3&details=ASMA0000288988']}>
-        <Details />
-      </MemoryRouter>
-    );
-    expect(screen.getByRole('img')).toHaveAttribute(
-      'src',
-      '/assets/images/spinner.gif'
-    );
-  });
-
-  it('correctly displays the detailed card data', () => {
-    mockedUseNavigation.mockReturnValue({ state: 'idle' });
-    render(
-      <MemoryRouter initialEntries={['/?page=3&details=ASMA0000288988']}>
-        <Details />
-      </MemoryRouter>
-    );
-    expect(screen.getByRole('img')).toHaveAttribute(
-      'src',
-      '/assets/images/star-system.png'
-    );
-    expect(screen.getByRole('link', { name: 'Close' })).toHaveAttribute(
-      'href',
-      '/?page=3'
-    );
-  });
-
-  it('clicking the close button hides the component', async () => {
-    const memoryRouter = createMemoryRouter(
-      [
+const memoryRouter = createMemoryRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
         {
           path: '/',
-          element: <App />,
+          element: <Outlet context={'ASMA0000288988'} />,
           children: [
             {
               path: '/',
               element: <Details />,
-              loader: vi.fn(() => astronomicalObject),
             },
           ],
         },
       ],
-      {
-        initialEntries: ['/?details=ASMA0000288988'],
-      }
+    },
+  ],
+  { initialEntries: ['/?page=3&details=ASMA0000288988'] }
+);
+
+describe('Details (Detailed Card) component', async () => {
+  it('correctly displays the detailed card data', async () => {
+    render(
+      <Provider store={store}>
+        <RouterProvider router={memoryRouter} />
+      </Provider>
     );
 
-    render(<RouterProvider router={memoryRouter} />);
+    await waitFor(() => {
+      const details = screen.getByTestId('details');
+      expect(details).toBeInTheDocument();
+      expect(
+        details.innerHTML.match('/assets/images/star-system.png')
+      ).toBeTruthy();
+      expect(details.innerHTML.match('ASMA0000288988')).toBeTruthy();
 
-    waitFor(() => {
-      const closeLink = screen.getByRole('link', { name: 'Close' });
-      fireEvent.click(closeLink);
-      expect(screen.getAllByTestId('details')).not.toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Close' })).toHaveAttribute(
+        'href',
+        '/?page=3'
+      );
     });
   });
-});
 
-describe('detailsLoader', async () => {
-  it('returns correct data', async () => {
-    const request = new Request(
-      'http://localhost:5173/?details=ASMA0000288988'
+  it('clicking the close button hides the component', async () => {
+    render(
+      <Provider store={store}>
+        <RouterProvider router={memoryRouter} />
+      </Provider>
     );
-    const response = await detailsLoader({ request });
-    expect(response).toHaveProperty('uid');
+
+    await waitFor(() => {
+      const details = screen.getByTestId('details');
+      expect(details).toBeInTheDocument();
+      const closeLink = screen.getByRole('link', { name: 'Close' });
+      fireEvent.click(closeLink);
+      expect(details).not.toBeInTheDocument();
+    });
   });
 
-  it('returns null when incorrect data', async () => {
-    const request = new Request('http://localhost:5173/');
-    const response = await detailsLoader({ request });
-    expect(response).toBe(null);
+  it('loading indicator is displayed while fetching data', async () => {
+    render(
+      <Provider store={store}>
+        <RouterProvider router={memoryRouter} />
+      </Provider>
+    );
+
+    server.events.on('request:start', () => {
+      expect(screen.getAllByTestId('loading')).toBeInTheDocument();
+    });
   });
 });
--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,9 +1,4 @@
-import {
-  NavLink,
-  useLocation,
-  useNavigation,
-  useOutletContext,
-} from 'react-router-dom';
+import { NavLink, useLocation, useOutletContext } from 'react-router-dom';
 import './Details.css';
 import Image from '../Item/Image';
 import Loading from '../_ui/bars/Loading/Loading';
@@ -11,39 +6,44 @@ import { useGetItemByIdQuery } from '../../services/api';
 
 export default function Details() {
   const [uid] = useOutletContext<string>();
-  const { data } = useGetItemByIdQuery(uid);
 
-  const navigation = useNavigation();
+  const { data, isLoading, isError, isSuccess } = useGetItemByIdQuery(uid);
 
   const location = useLocation();
   const closeLink = new URLSearchParams(location.search);
   if (closeLink.has('details')) closeLink.delete('details');
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if  (isError) {
+    return <div>Error While Getting Details Data</div>
+  }
+
   return (
     <div className="details" data-testid="details">
-      {navigation.state === 'loading' ? (
-        <Loading />
-      ) : (
+      {isSuccess && data && (
         <div className="details__content">
-          {data?.astronomicalObject.astronomicalObjectType && (
+          {data.astronomicalObject.astronomicalObjectType && (
             <Image
               astronomicalObjectType={
                 data.astronomicalObject.astronomicalObjectType
               }
             />
           )}
-          {data?.astronomicalObject.name && <div>Details:</div>}
-          {data?.astronomicalObject.name && (
-            <div>Name: {data?.astronomicalObject.name}</div>
+          {data.astronomicalObject.name && <div>Details:</div>}
+          {data.astronomicalObject.name && (
+            <div>Name: {data.astronomicalObject.name}</div>
           )}
           {data?.astronomicalObject.astronomicalObjectType && (
-            <div>Type: {data?.astronomicalObject.astronomicalObjectType}</div>
+            <div>Type: {data.astronomicalObject.astronomicalObjectType}</div>
           )}
-          {data?.astronomicalObject.location?.name && (
-            <div>Location: {data?.astronomicalObject.location?.name}</div>
+          {data.astronomicalObject.location?.name && (
+            <div>Location: {data.astronomicalObject.location?.name}</div>
           )}
-          {data?.astronomicalObject.uid && (
-            <div>ID: {data?.astronomicalObject.uid}</div>
+          {data.astronomicalObject.uid && (
+            <div>ID: {data.astronomicalObject.uid}</div>
           )}
           <NavLink to={`?${closeLink.toString()}`}>Close</NavLink>
         </div>
