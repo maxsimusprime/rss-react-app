da70a38025f6949e6354fa896dd4ca9d27261fc9
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-11-26 20:52:57 +0300

    feat: update Pagination test

--- src/__test__/Pagination.test.tsx
+++ src/__test__/Pagination.test.tsx
@@ -1,69 +1,47 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it } from 'vitest';
 import Pagination from '../components/Pagination/Pagination';
-import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { page } from '../mocks/objects';
-import { Page } from '../dto/types';
+import mockRouter from 'next-router-mock';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 
-const setupMemoryRouter = (page: Page) =>
-  createMemoryRouter(
-    [
-      {
-        path: '/',
-        element: <Pagination page={page} />,
-        children: [],
-      },
-    ],
-    { initialEntries: ['/?page=0'] }
-  );
+vi.mock('next/router', async () => await vi.importActual('next-router-mock'));
 
 describe('Pagination component', () => {
-  it('updates URL query parameter when page changes', async () => {
-    const memoryRouter = setupMemoryRouter(page);
-    render(<RouterProvider router={memoryRouter} />);
-
-    const searchParams = new URLSearchParams(
-      memoryRouter.state.location.search
-    );
-
-    const initialPageNumber = Number(searchParams.get('page'));
-
+  it('renders correctly', () => {
+    mockRouter.push('/?pageSize=0&pageNumber=0');
+    render(<Pagination {...page} />, { wrapper: MemoryRouterProvider });
     expect(screen.getByTestId('pagination')).toBeInTheDocument();
-    expect(initialPageNumber).toBe(0);
+  });
+
+  it('updates URL query parameter when page changes', async () => {
+    mockRouter.push('/?pageSize=0&pageNumber=0');
+    render(<Pagination {...page} />, { wrapper: MemoryRouterProvider });
 
     const nextPageLink = screen.getByRole('link', { name: '>' });
-    expect(nextPageLink.getAttribute('href')).toBe('/?page=1');
-    fireEvent.click(nextPageLink);
+    expect(nextPageLink.getAttribute('href')).toBe(
+      '/?pageSize=10&pageNumber=1'
+    );
 
-    await waitFor(() => {
-      expect(screen.getByTestId('pagination')).toBeInTheDocument();
-      const newSearchParams = new URLSearchParams(
-        memoryRouter.state.location.search
-      );
-      const newPageNumber = newSearchParams.get('page');
-      expect(Number(newPageNumber)).toBe(1);
-    });
+    fireEvent.click(nextPageLink);
+    expect(mockRouter.asPath).toBe('/?pageSize=10&pageNumber=1');
   });
 
   it('left arrow link disabled because first page', () => {
-    const memoryRouter = setupMemoryRouter(page);
-    render(<RouterProvider router={memoryRouter} />);
-
-    expect(screen.getByTestId('pagination')).toBeInTheDocument();
+    mockRouter.push('/?pageSize=0&pageNumber=0');
+    render(<Pagination {...page} />, { wrapper: MemoryRouterProvider });
 
     const prevPageLink = screen.getByText('<');
     expect(prevPageLink instanceof HTMLSpanElement).toBeTruthy();
   });
 
   it('right arrow link enabled because first page', () => {
-    const memoryRouter = setupMemoryRouter(page);
-    render(<RouterProvider router={memoryRouter} />);
-
-    expect(screen.getByTestId('pagination')).toBeInTheDocument();
+    mockRouter.push('/?pageSize=10&pageNumber=0');
+    render(<Pagination {...page} />, { wrapper: MemoryRouterProvider });
 
     const nextPageLink = screen.getByRole('link', { name: '>' });
-    expect(nextPageLink.getAttribute('href')).toBe('/?page=1');
+    expect(nextPageLink instanceof HTMLAnchorElement).toBeTruthy();
   });
 
   it('left arrow link enabled because last page', () => {
@@ -71,13 +49,12 @@ describe('Pagination component', () => {
       ...page,
       ...{ firstPage: false, lastPage: true, pageNumber: 2 },
     });
-    const memoryRouter = setupMemoryRouter(lastPage);
-    render(<RouterProvider router={memoryRouter} />);
 
-    expect(screen.getByTestId('pagination')).toBeInTheDocument();
+    mockRouter.push('/?pageSize=10&pageNumber=1');
+    render(<Pagination {...lastPage} />, { wrapper: MemoryRouterProvider });
 
     const prevPageLink = screen.getByRole('link', { name: '<' });
-    expect(prevPageLink.getAttribute('href')).toBe('/?page=1');
+    expect(prevPageLink instanceof HTMLAnchorElement).toBeTruthy();
   });
 
   it('right arrow link disabled because last page', () => {
@@ -85,8 +62,9 @@ describe('Pagination component', () => {
       ...page,
       ...{ firstPage: false, lastPage: true, pageNumber: 2 },
     });
-    const memoryRouter = setupMemoryRouter(lastPage);
-    render(<RouterProvider router={memoryRouter} />);
+
+    mockRouter.push('/?pageSize=10&pageNumber=1');
+    render(<Pagination {...lastPage} />, { wrapper: MemoryRouterProvider });
 
     const nextPageLink = screen.getByText('>');
     expect(nextPageLink instanceof HTMLSpanElement).toBeTruthy();
