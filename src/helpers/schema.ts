12849bd33befede49d4ebd2c586dc79716e13e27
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2023-12-03 20:17:34 +0300

    feat: create yup schema

---
+++ src/helpers/schema.ts
@@ -0,0 +1,58 @@
+import * as yup from 'yup';
+
+const MAX_FILE_SIZE = 1024 * 100;
+
+export const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Name is required')
+    .matches(/^[A-Z]{1}[a-zA-Z]*$/, 'First letter must be a Uppercased'),
+
+  age: yup
+    .string()
+    .required('Age is required')
+    .matches(/^\d+$/, 'Only positive number'),
+
+  email: yup
+    .string()
+    .required('Email is required')
+    .matches(
+      /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}/,
+      'Invalid email format'
+    ),
+
+  password: yup.string().required('Password is required'),
+
+  passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Doesn`t match with password field')
+    .required('Password re-input is required'),
+
+  country: yup.string().required('Country is required'),
+
+  gender: yup.string().required('Gender choice is required'),
+
+  photo: yup
+    .mixed()
+    .nullable()
+    .test(
+      'is-file-added',
+      'Photo is required',
+      (files) => files instanceof FileList && files.length > 0
+    )
+    .test(
+      'is-single-file',
+      'Only one photo is required',
+      (files) => files instanceof FileList && files.length === 1
+    )
+    .test(
+      'is-valid-size',
+      'Max allowed size of photo is 100 KBytes',
+      (files) =>
+        files instanceof FileList && files[0] && files[0].size <= MAX_FILE_SIZE
+    ),
+
+  accepted: yup
+    .string()
+    .test('is-confirmed', 'Needs to confirm', (value) => value === 'true'),
+});
